cd71b087c81473f8b22280556327620326b6c227
KB <kj.br@protonmail.com>
2023-02-11 22:09:46 -0600

    Update Categories Preview

---
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -0,0 +1,23 @@
+import { Fragment, useContext } from "react"
+import CategoryPreview from "../../components/category-preview/CategoryPreview.component"
+import { CategoriesContext } from "../../contexts/categories.context"
+
+
+const CategoriesPreview = () => {
+
+    const {categoriesMap} = useContext(CategoriesContext)
+    const categoryArray = Object.entries(categoriesMap)
+    
+  return (
+    <div className="category-preview-container">
+
+      {categoryArray.map(category=>
+        (
+          <CategoryPreview key={category[0]} title={category[0]} products={category[1]}/>
+        )
+      )}
+    </div>
+  )
+}
+
+export default CategoriesPreview
