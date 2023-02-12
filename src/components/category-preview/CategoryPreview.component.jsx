e53e02c5fa213d384fa7027f2cda250a794c7d14
KB <kj.br@protonmail.com>
2023-02-12 14:33:29 -0600

    Conversion to Styled Components`

--- src/components/category-preview/CategoryPreview.component.jsx
+++ src/components/category-preview/CategoryPreview.component.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
 import ProductCard from '../product-card/product-card.component'
 import './category-preview.scss'
 
@@ -20,7 +21,7 @@ const CategoryPreview = ({title, products}) => {
     return (
         <div className='category-preview-container'>
             <h2>
-                <span className='title'>{title.toUpperCase()}</span>
+                <Link className='title' to={title}>{title.toUpperCase()}</Link>
             </h2>
             <div className='preview'>
                 {  products.filter((_, idx)=> randomProductIdx.has(idx))
