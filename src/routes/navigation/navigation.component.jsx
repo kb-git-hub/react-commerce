3df75205eb633119d93e7c3d9af7830ed22eba18
KB <kj.br@protonmail.com>
2023-02-14 17:35:26 -0600

    Update NavLinks

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -23,7 +23,7 @@ const Navigation = () =>{
             <NavLinks>
               <NavLink to={'/shop'}>SHOP</NavLink>
                 {
-                  currentUser ?  (<NavLink onClick={signOutUser}>SIGN OUT</NavLink>) : 
+                  currentUser ?  (<NavLink as='span' onClick={signOutUser}>SIGN OUT</NavLink>) : 
                   (<NavLink to={'/auth'}>SIGN IN</NavLink>)
                 }
               <CartIcon/>
